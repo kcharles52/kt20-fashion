b71296c1783514595face66b8f8a52b632590af1
Kato Charles <kcharles52@gmail.com>
2021-11-11 09:15:21 +0300

    - add general navigation

--- App.js
+++ App.js
@@ -7,37 +7,10 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import AppNavContainer from './src/navigations';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Kt 20 fashions</Text>
-    </SafeAreaView>
-  );
+  return <AppNavContainer></AppNavContainer>;
 };
 
 export default App;
---
+++ src/navigations/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+const AppNavContainer = () => {
+  return (
+    <NavigationContainer>
+      <Text>Kt 20 fashions</Text>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavContainer;
