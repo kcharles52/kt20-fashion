e8ca0f71829f0c47d724af92ebe86401ececc6e9
Kato Charles <kcharles52@gmail.com>
2021-11-18 13:17:53 +0300

    - fix creation of drawer

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useContext} from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeNavigator from './HomeNavigator';
 import {HOME_NAVIGATOR} from '../constants/routeNames';
+import SideMenu from './SideMenu';
+import {GlobalContext} from '../context/Provider';
 
-const Drawer = createNativeStackNavigator();
+const getDrawerContent = (navigation, authDispatch) => {
+  return <SideMenu navigation={navigation} authDispatch={authDispatch} />;
+};
+
+const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
+  const {authDispatch} = useContext(GlobalContext);
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerType="slide"
+      drawerContent={({navigation}) =>
+        getDrawerContent(navigation, authDispatch)
+      }>
       <Drawer.Screen name={HOME_NAVIGATOR} component={HomeNavigator} />
     </Drawer.Navigator>
   );
